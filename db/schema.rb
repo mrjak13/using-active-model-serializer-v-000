cb5ae043692e2832ed324f49556f3c3232e3c834
Justin Kenney <justinkenney13@gmail.com>
2019-05-06 18:59:18 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119185429) do
+ActiveRecord::Schema.define(version: 20190506185733) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20160119185429) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "author_id"
+  end
+
+  create_table "somethings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
